e03a29bf3863637640775c6d3e5d07e1e80a3c16
Diorgeles Dias Lima <diorgelesdias@gmail.com>
2017-12-06 09:44:46 -0200

    Corrigido classe de resposta do IMC

--- Imc.cs
+++ Imc.cs
@@ -9,25 +9,27 @@ namespace ConsoleApplication
 
         public string classificacao(float imc)
         {
+            var mensagem = "";
+            
             if (imc <= 18.5)
-                return "Abaixo do peso";
+                mensagem="Abaixo do peso";
 
             else if ((imc > 18.5) && (imc < 25))
-                return "Peso ideal (Parabéns)";
+                mensagem="Peso ideal (Parabéns)";
 
             else if ((imc >= 25) && (imc < 30))
-                return "Levemente acima do peso";
+                mensagem="Levemente acima do peso";
 
             else if ((imc >= 30) && (imc < 35))
-                return "Obesidade grau I";
+                mensagem="Obesidade grau I";
 
             else if ((imc >= 35) && (imc < 40))
-                return "Obesidade grau II (severa)";
+                mensagem="Obesidade grau II (severa)";
 
             else if (imc >= 40)
-                return "Obesidade Grau III (mórbida)";
+                mensagem="Obesidade Grau III (mórbida)";
 
-            return string.Empty; // Esse return está aqui pois é necessário, mas ele nunca irá chegar aqui.
+            return mensagem;
         }
         
     }
